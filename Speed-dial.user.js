22165f1d5f5a440573f833dc35fe15999410ed36
Extension-404 <b7627934@gmail.com>
2026-07-28 19:15:50 +0300

    Update version to 2.5 in Speed-dial.user.js

--- Speed-dial.user.js
+++ Speed-dial.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         חיוג מהיר ומערכת צינתוקים - ימות המשיח (PRO Security)
 // @namespace    http://tampermonkey.net/
-// @version      2.4.1
+// @version      2.5
 // @description  איתור מספרי טלפון, הצפנת טוקן, סנכרון זיהוי יוצא, כפתורי בחר/נקה הכל וחסימת חיוג עצמי בחלונות התוסף
 // @match        *://*/*
 // @exclude      *://docs.google.com/*
